e7000b94e344ec719e1d3ce5a44ddd59727f888a
Armando Junior <72458893+ArmandoJunior00@users.noreply.github.com>
2025-04-09 17:05:44 -0300

    Create semafaro.cpp

---
+++ semafaro.cpp
@@ -0,0 +1,28 @@
+int ledRed=8;
+int ledYellow=9;
+int ledGreen=10;
+
+void setup() {
+  pinMode(ledRed,OUTPUT);
+  pinMode(ledYellow,OUTPUT);
+  pinMode(ledGreen,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(ledRed,HIGH);
+  digitalWrite(ledYellow,LOW);
+  digitalWrite(ledGreen,LOW);
+  delay(7000);
+
+  digitalWrite(ledRed,LOW);
+  digitalWrite(ledYellow,LOW);
+  digitalWrite(ledGreen,HIGH);
+  delay(9000);
+
+  
+  digitalWrite(ledRed,LOW);
+  digitalWrite(ledYellow,HIGH);
+  digitalWrite(ledGreen,LOW);
+  delay(2000);
+}
