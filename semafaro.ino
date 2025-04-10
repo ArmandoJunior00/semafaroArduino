c3ba2938d20fd0599af7c12c40180136a93638ae
Armando Junior <72458893+ArmandoJunior00@users.noreply.github.com>
2025-04-10 16:12:55 -0300

    refatorando semafaro.ino

--- semafaro.ino
+++ semafaro.ino
@@ -1,28 +1,40 @@
-int ledRed=8;
-int ledYellow=9;
-int ledGreen=10;
+#define ledRed 8
+#define ledYellow 9
+#define ledGreen 10
+
 
 void setup() {
   pinMode(ledRed,OUTPUT);
   pinMode(ledYellow,OUTPUT);
   pinMode(ledGreen,OUTPUT);
-
 }
 
-void loop() {
+  void red(int time){
   digitalWrite(ledRed,HIGH);
   digitalWrite(ledYellow,LOW);
   digitalWrite(ledGreen,LOW);
-  delay(7000);
+  delay(time*1000);
+  }
 
+  void green(int time){
   digitalWrite(ledRed,LOW);
   digitalWrite(ledYellow,LOW);
   digitalWrite(ledGreen,HIGH);
-  delay(9000);
+  delay(time*1000);
+  }
 
-  
+  void yellow(int time){
   digitalWrite(ledRed,LOW);
   digitalWrite(ledYellow,HIGH);
   digitalWrite(ledGreen,LOW);
-  delay(2000);
+  delay(time*1000);
+  }
+
+
+void loop() {
+
+red(9);
+green(7);
+yellow(2);
+ 
 }
